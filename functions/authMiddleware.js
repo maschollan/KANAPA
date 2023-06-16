acf1dd5a8a9f2431f742c88e2b5fa4d9a8b72e8a
mrahmatmuhaimin <muhaiminrahmat@gmail.com>
2023-06-16 08:41:20 +0700

    Add Images Endpoint

--- functions/authMiddleware.js
+++ functions/authMiddleware.js
@@ -1,19 +1,65 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-const authenticateToken = async (req, res, next) => {
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = async (req, res, next) => {
+  functions.logger.log("Check if request is authorized with Firebase ID token");
+
+  if (
+    (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) &&
+    !(req.cookies && req.cookies.__session)
+  ) {
+    functions.logger.error(
+        "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+        "Make sure you authorize your request by providing the following HTTP header:",
+        "Authorization: Bearer <Firebase ID Token>",
+        "or by passing a \"__session\" cookie.",
+    );
+    res.status(403).send({
+      error: true,
+      message: `Unauthorized`,
+    });
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+    functions.logger.log("Found \"Authorization\" header");
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else if (req.cookies) {
+    functions.logger.log("Found \"__session\" cookie");
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send({
+      error: true,
+      message: `Unauthorized`,
+    });
+    return;
+  }
+
   try {
-    const authToken = req.headers.authorization;
-    if (!authToken || !authToken.startsWith("Bearer ")) {
-      return res.status(401).json({error: true, message: "Unauthorized"});
-    }
-    const idToken = authToken.split(" ")[1];
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    req.user = decodedToken;
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log("ID Token correctly decoded", decodedIdToken);
+    req.user = decodedIdToken;
     next();
+    return;
   } catch (error) {
-    console.error("Error verifying ID token:", error);
-    return res.status(401).json({error: true, message: "Unauthorized"});
+    functions.logger.error("Error while verifying Firebase ID token:", error);
+    res.status(403).send({
+      error: true,
+      message: `Unauthorized`,
+    });
+    return;
   }
 };
-module.exports = authenticateToken;
+
+module.exports = validateFirebaseIdToken;
--- functions/index.js
+++ functions/index.js
@@ -7,26 +7,26 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const Multer = require("multer");
-const {v4: uuidv4} = require("uuid");
+const multer = require("multer");
+const {v4: uuidv4} = require('uuid');
 const fs = require("fs");
 const path = require("path");
-const {Storage} = require("@google-cloud/storage");
 const bodyParser = require('body-parser');
 
 
 const app = express();
 app.use(cors({origin: true}));
+const authMiddleware = require("./authMiddleware");
 
 
-const serviceAccount = require("./serviceAccountKey.json");
-const storageBucket = 'gs://capstone-kanapa.appspot.com/';
+// const serviceAccount = require("./serviceAccountKey.json");
+// const storageBucket = 'capstone-kanapa.appspot.com';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: storageBucket,
-});
-const bucket = admin.storage().bucket();
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: storageBucket,
+// });
+// const bucket = admin.storage().bucket();
 // const db = admin.firestore();
 
 // Register endpoint
@@ -94,84 +94,90 @@ app.post("/login", async (req, res) => {
 });
 
 // Add New Image With Authentication
-const multer = Multer({
-  storage: Multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, path.join(__dirname, 'public/uploads'));
-    },
-    filename: function(req, file, cb) {
-      cb(
-          null,
-          file.fieldname + '-' + Date.now() + path.extname(file.originalname),
-      );
-    },
-  }),
-  limits: {
-    fileSize: 5 * 1024 * 1024, // Batasan ukuran file (5 MB)
+const {Storage} = require('@google-cloud/storage');
+const service = require("./serviceAccountKey.json"); // path to service.json file obtained from firebase server for firebase admin access
+const {
+  fileParser,
+} = require('express-multipart-file-parser'); //
+app.use(fileParser({
+  rawBodyOptions: {
+    limit: '15mb', // file size limit
   },
+  busboyOptions: {
+    limits: {
+      fields: 20, // Number text fields allowed
+    },
+  },
+}));
+
+admin.initializeApp({
+  credential: admin.credential.cert(service), // service.json
+  databaseURL: "https://console.firebase.google.com/project/capstone-kanapa/firestore/data/~2F", // DATABASE_URL
+});
+const storage = new Storage();
+const bucket = storage.bucket("gs://capstone-kanapa.appspot.com"); // BUCKET_URL from firebase console
+app.post('/images', authMiddleware, (req, res) => {
+  const {
+    fieldname,
+    originalname,
+    encoding,
+    mimetype,
+    buffer,
+  } = req.files[0];
+  if (req.files[0]) {
+    uploadImageToStorage(req.files[0]).then((url) => {
+      const downloadUrl = getDownloadUrl(url);
+      res.send({status: true, data: "Image uploaded successfully", downloadUrl});
+    })
+        .catch((err)=>{
+          res.send({status: false,
+            data: "Post failed to add"});
+        });
+  } else {
+    res.send('image not provided');
+  }
 });
 
-// Middleware untuk memeriksa token Firebase
-const verifyFirebaseToken = (req, res, next) => {
-  const authorizationHeader = req.headers.authorization;
+// /**
+// * Upload the image file to Google Storage
+// * @param {File} file object that will be uploaded to Google Storage
+// */
+const uploadImageToStorage = (file) => {
+  const prom = new Promise((resolve, reject) => {
+    if (!file) {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('No image file');
+    }
+    const newFileName = uuidv4() + file.originalname; // uqiue name
 
-  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-    return res.status(401).json({error: true, message: 'Unauthorized'});
-  }
+    const fileUpload = bucket.file(newFileName);
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
 
-  const token = authorizationHeader.split('Bearer ')[1];
+    blobStream.on('error', (error) => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('Something is wrong! Unable to upload at the moment.');
+    });
 
-  admin
-      .auth()
-      .verifyIdToken(token)
-      .then((decodedToken) => {
-        req.userId = decodedToken.uid;
-        next();
-      })
-      .catch((error) => {
-        console.error('Error verifying Firebase token:', error);
-        return res.status(401).json({error: true, message: 'Unauthorized'});
-      });
+    blobStream.on('finish', () => {
+      const url = `https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`; // image url from firebase server
+      resolve(url);
+    });
+
+    blobStream.end(file.buffer);
+  });
+  return prom;
 };
 
-// Endpoint untuk menambahkan gambar baru (memerlukan autentikasi Firebase)
-app.post('/images', verifyFirebaseToken, multer.single('photo'), (req, res) => {
-  const {description, lat, lon} = req.body;
-  const file = req.file;
-
-  // Validasi input
-  if (!description || !file) {
-    return res.status(400).json({error: true, message: 'Description and photo are required'});
-  }
-
-  // Generate nama file unik
-  const fileName = `${Date.now()}_${file.originalname}`;
-
-  // Upload file ke Firebase Storage
-  const uploadFile = bucket.file(fileName);
-  const blobStream = uploadFile.createWriteStream({
-    metadata: {
-      contentType: file.mimetype,
-    },
-  });
-
-  blobStream.on('error', (error) => {
-    console.error(error);
-    return res.status(500).json({error: true, message: 'An error occurred while uploading the file'});
-  });
-
-  blobStream.on('finish', () => {
-    // Dapatkan URL publik untuk file yang diunggah
-    const publicUrl = `https://storage.googleapis.com/${storageBucket}/${uploadFile.name}`;
-
-    // Lakukan operasi lain yang diperlukan, misalnya menyimpan data ke database
-
-    // Kirim respons berhasil
-    return res.json({error: false, message: 'success'});
-  });
-
-  blobStream.end(file.buffer);
-});
+const getDownloadUrl = (imageUrl) => {
+  // Lakukan proses atau manipulasi URL gambar sesuai kebutuhan
+  const fileName = imageUrl.split('/').pop(); // Mengambil nama file dari URL
+  const downloadUrl = `https://storage.cloud.google.com/${bucket.name}/${fileName}`; // Contoh: Mengganti domain dan menambahkan direktori unduhan
+  return downloadUrl;
+};
 
 // exports the api to firebase cloud functions
 exports.app = functions.https.onRequest(app);
